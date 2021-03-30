617e8e369fa596375ced25d3ee47fb3251f4b323
Derek Roode <derek.i.roode@gmail.com>
2021-03-30 01:38:10 -0400

    db setup good

--- app/models/users_groups.py
+++ app/models/users_groups.py
@@ -5,3 +5,12 @@ class User_Group(db.Model):
     __tablename__ = 'user_groups'
 
     id = db.Column(db.Integer, primary_key=True)
+    users_id = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+    groups_id = db.Column(db.Integer, ForeignKey("groups.id", nullable=False))
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "users_id" = self.users_id,
+            "groups_id" = self.groups_id
+        }
