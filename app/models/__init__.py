1c3153ab91a40ed8ac627e5218954379debdc8a2
Derek Roode <derek.i.roode@gmail.com>
2021-03-30 01:40:34 -0400

    updated init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,10 @@
 from .db import db
 from .user import User
+from .action_shots import Action_Shot
+from .comments import Comment
+from .groups import Group
+from .progress_photos import Progress_Photo
+from .stock_workouts import Stock_Workout
+from .user_workouts import User_Workout
+from .users_groups import User_Group
+
