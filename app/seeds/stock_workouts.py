a2229d686814355855014353439b7c739dd8ea44
Derek Roode <derek.i.roode@gmail.com>
2021-04-08 17:20:33 -0400

    added workout seed

--- app/seeds/stock_workouts.py
+++ app/seeds/stock_workouts.py
@@ -7,8 +7,10 @@ def seed_stock_workouts():
                              audio_url='https://boxit.s3.us-east-2.amazonaws.com/workouts/Workout1-compressed.m4v')
     workout2 = Stock_Workout(coach_photo_url='https://boxit.s3.us-east-2.amazonaws.com/coaches/mary.PNG',
                              audio_url='https://boxit.s3.us-east-2.amazonaws.com/workouts/Workout2-compressed.m4v')
+    workout3 = Stock_Workout(coach_photo_url='https://boxit.s3.us-east-2.amazonaws.com/coaches/cody.PNG',
+                             audio_url='https://boxit.s3.us-east-2.amazonaws.com/workouts/Workout3-compressed.m4v')
 
-    workouts = [workout1, workout2]
+    workouts = [workout1, workout2, workout3]
     for workout in workouts:
         db.session.add(workout)
 
