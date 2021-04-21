58df9153b5fc99324317a5bdff7522fca884183a
Derek Roode <derek.i.roode@gmail.com>
2021-04-20 20:12:48 -0400

    progress photos store set up and page workign

--- react-app/src/components/ProgressPhotos/ProgressPhotos.js
+++ react-app/src/components/ProgressPhotos/ProgressPhotos.js
@@ -1,15 +1,76 @@
-import React from "react";
-import  "./ProgressPhotos.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./ProgressPhotos.css";
+
+import { progress_photo, getProgressPhotos } from "../../store/ProgressPhotos";
 
 export default function ProgressPhotos() {
+  const dispatch = useDispatch();
+ const photos = useSelector((state)=> state?.progressPhoto.progress_photo)
+  const [progressPhoto, setProgressPhoto] = useState("");
+  console.log('1',photos);
+
+  useEffect(() => {
+    dispatch(getProgressPhotos());
+   }, [dispatch]);
+
+
+  // add new progress photo
+  const addProgressPhoto = async (e) => {
+    e.preventDefault();
+    const photo = await dispatch(
+      progress_photo({
+        progressPhoto,
+      })
+    );
+    await dispatch(getProgressPhotos());
+    return photo;
+  };
+
+  // set progress photos and render
+  const updateProgressPhoto = (e) => {
+    setProgressPhoto(e.target.files[0]);
+  };
+
+  const getMyProgressPhotos = () => {
+    let pic = photos.map((photo, index) => {
+      console.log(photo);
+
+      return (
+        <div className="action_photos" key={index}>
+          <img src={photo.photo_url} />
+        </div>
+      );
+    });
+    // let pic = 1
+    return pic;
+
+  };
+
   return (
     <div className="progress_photos__container">
-      <h2 id='test'> test </h2>
-      <div className="progress_photos__user_images">
-        <img src="https://cdn.shopify.com/s/files/1/2224/2181/files/BSL_DomMountatin_square_for_site_x800.jpg?v=1574552341" />
+      <h2> Your Progress </h2>
+      <h3>Check out your progress here</h3>
+      <div className="progress_photos-form-container">
+
+        <form onSubmit={addProgressPhoto} className="actionShots-form">
+          <div>
+            <input
+              className="progress_photos-form-photo"
+              type="file"
+              name="progess photo"
+              onChange={updateProgressPhoto}
+              required={true}
+            ></input>
+          </div>
+          <div>
+            <button type="submit">Add Your Progress Photo</button>
+          </div>
+        </form>
       </div>
-      <div className="progress_photos__user_images">
-        <img src="https://imgix.ranker.com/list_img_v2/4526/2084526/original/youtube-broscience-videos?w=817&h=427&fm=jpg&q=50&fit=crop" />
+      <div className="progress_photos-container">
+
+        {photos ? getMyProgressPhotos() : "Add A Photo"}
       </div>
     </div>
   );
