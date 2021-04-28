1e14dded6bb372d586a66c5219243bdba50766c9
Derek Roode <derek.i.roode@gmail.com>
2021-04-27 20:49:41 -0400

    about page

--- react-app/src/components/About/About.js
+++ react-app/src/components/About/About.js
@@ -1,13 +1,91 @@
 import React from "react";
+import derek from '../../images/derek.jpg'
+import { Redirect, NavLink } from "react-router-dom";
+
 
 import "./About.css";
 
- const About = () => {
+const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-    </div>
-  )
-}
+    <div className="about_container">
 
-export default About
+      <div className="about_container-header">
+
+        {/* TOP SECTION 1 */}
+        <div className="about_container-header-2">
+          <div className="about_container-header-2-top">
+            <div className="about_container-header-profilePhoto">
+              <div>
+                <img src={derek} />
+              </div>
+              <div className="aboutname">
+                <h2>Derek Roode</h2>
+              </div>
+            </div>
+            {/* TOP SECTION 2 */}
+            <div className="about_container-header-aboutStats">
+              <div className="about_container-header-ContentBox">
+                <div id="overallRank">
+                  <div>
+                    <i className="fas fa-trophy"></i>
+                  </div>
+                  <div>
+                    <h3>Full Stack Software Engineer</h3>
+                  </div>
+ 
+                </div>
+
+                <div id="workoutsCompleted">
+                  <div>
+                    <i className="fas fa-dumbbell"></i>
+                  </div>
+                  <div>
+                    <h3>Written In React / Redux / Flask</h3>
+                  </div>
+
+                </div>
+
+                <div id="hoursCompleted">
+                  <div>
+                    <i className="fas fa-clock"></i>
+                  </div>
+                  <div>
+                    <h3>Completed in 2021</h3>
+                  </div>
+
+                </div>
+
+                <div id="currentLevel">
+                  <div>
+                    <i className="fas fa-sliders-h"></i>
+                  </div>
+                  <div>
+                    <h3>Open to Offers</h3>
+                  </div>
+                  <div>
+                    <div id='currentLevel-text'>Yes</div>
+                  </div>
+                </div>
+                <div id="startWorkout">
+                  <a href={'http://derekroode.com'}
+                                      activeClassName="active"
+                                      className="links-each"
+                  >
+                  View My Portfolip Page
+
+                  </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default About;
