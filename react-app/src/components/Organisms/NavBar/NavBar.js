f313320bed5805b28c09a85cad53f65b30bc469b
Derek Roode <droode@nexient.com>
2021-11-23 10:34:28 -0500

    navbar desktop complete

--- react-app/src/components/Organisms/NavBar/NavBar.js
+++ react-app/src/components/Organisms/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../../Atoms/Logo/Logo'
 import NavLinks from '../../Atoms/NavLinks/NavLinks'
 import NavBarLinks from '../../Molecules/NavBarLinks/NavBarLinks'
 import styles from './NavBar.module.css'
@@ -6,6 +7,7 @@ import styles from './NavBar.module.css'
 function NavBar() {
   return (
     <div className={styles.navbar_container}>
+      <Logo/>
       <NavBarLinks/>
   
     </div>
