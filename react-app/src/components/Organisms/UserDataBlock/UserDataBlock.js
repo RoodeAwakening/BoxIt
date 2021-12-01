2238754c3377986c3b10922fa71692312e49212b
Derek Roode <droode@nexient.com>
2021-11-30 23:23:04 -0500

    mobile status bar containers

--- react-app/src/components/Organisms/UserDataBlock/UserDataBlock.js
+++ react-app/src/components/Organisms/UserDataBlock/UserDataBlock.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import UserProfilePhotoBlock from '../UserProfilePhotoBlock/UserProfilePhotoBlock'
-import UserStats from '../../Molecules/UserStats/UserStats';
-import styles from './UserDataBlock.module.css'
-import StartWorkout from '../../Atoms/StartWorkout/StartWorkout';
+import UserProfilePhotoBlock from "../UserProfilePhotoBlock/UserProfilePhotoBlock";
+import UserStats from "../../Molecules/UserStats/UserStats";
+import styles from "./UserDataBlock.module.css";
+import StartWorkout from "../../Atoms/StartWorkout/StartWorkout";
 
 function UserDataBlock() {
   const sessionUser = useSelector((state) => state.session.user);
   return (
     <div className={styles.UserDataBlock_container}>
-      
-
-      <UserProfilePhotoBlock profilePhoto={sessionUser.profile_photo} userName={sessionUser.user_name}/>
-      <StartWorkout/>
-      
-      
-      
+      <div>
+        <UserProfilePhotoBlock
+          profilePhoto={sessionUser.profile_photo}
+          userName={sessionUser.user_name}
+        />
+      </div>
+      <div className={styles.UserDataBlock_container__right}>
+        <StartWorkout />
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserDataBlock
+export default UserDataBlock;
