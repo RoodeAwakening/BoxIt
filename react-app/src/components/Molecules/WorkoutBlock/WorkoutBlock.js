12d2aa9433628752735a322ea75e04c0dd625527
Derek Roode <droode@nexient.com>
2021-12-02 12:53:58 -0500

    video player now working to play random videos

--- react-app/src/components/Molecules/WorkoutBlock/WorkoutBlock.js
+++ react-app/src/components/Molecules/WorkoutBlock/WorkoutBlock.js
@@ -1,36 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import WorkoutVideo from '../../Atoms/WorkoutVideo/WorkoutVideo'
+import WorkoutVideo from "../../Atoms/WorkoutVideo/WorkoutVideo";
 
-import { allWorkouts } from '../../../store/workouts';
+import { allWorkouts } from "../../../store/workouts";
 
-import styes from './WorkoutBlock.module.css'
+import styes from "./WorkoutBlock.module.css";
 
-function WorkoutBlock() {
+function WorkoutBlock({ workout }) {
   const workouts = useSelector((state) => state.workout);
-  console.log('workouts',workouts);
+  console.log("workouts", workouts);
   const dispatch = useDispatch();
 
-
-
-
-  useEffect( () => {
-
+  useEffect(() => {
     dispatch(allWorkouts());
-
   }, []);
 
-  const logWorkout = () =>{
-    return null
-  }
+  // get random video to play
+  const workoutVideo = () => {
+    let num = 0;
+    for (let each in workouts) {
+      num += 1;
+    }
+    // get a random workout
+    let random = Math.floor(Math.random() * num + 1);
 
+    let randomWorkout = workouts[random];
 
-  return (
-    <div>
-      <WorkoutVideo video={workouts[2]?.audio_url} onEnd={logWorkout(workouts[2])}/>
-      
-    </div>
-  )
+    return (
+      <div>
+        <WorkoutVideo
+          video={randomWorkout?.audio_url}
+          onEnd={logWorkout(workouts[2])}
+        />
+      </div>
+    );
+  };
+
+  const logWorkout = () => {
+    return null;
+  };
+
+  return <div>{workouts ? workoutVideo() : "loading..."}</div>;
 }
 
-export default WorkoutBlock
+export default WorkoutBlock;
